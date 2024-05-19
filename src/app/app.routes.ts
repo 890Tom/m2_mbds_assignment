a4af2411e72bd8711c4af7ce20b8b84f800939c7
60 - Tommy ZAFITSIARENDRIKA <zafi.tommy@gmail.com>
2024-05-19 17:24:56 +0300

    Install tailwind

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AssignmentsComponent } from './assignments/assignments.component';
+import { SigninComponent } from './signin/signin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: AssignmentsComponent},
+    {path: 'signin', component: SigninComponent}
+];
+    
