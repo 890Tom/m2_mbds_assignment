4108982a3204ca98e142144bc7c8e058e88459a1
60 - Tommy ZAFITSIARENDRIKA <zafi.tommy@gmail.com>
2024-05-21 20:07:17 +0300

    [add] debut d'ajout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import { AssignmentsComponent } from './assignments/assignments.component';
 import { SigninComponent } from './signin/signin.component';
 import { authGuard } from './shared/guard/auth.guard';
-
+import {AddAssignmentComponent} from './assignments/add-assignment/add-assignment.component';
 export const routes: Routes = [
     {path: '', component: AssignmentsComponent, canActivate: [authGuard]},
-    {path: 'signin', component: SigninComponent}
+    {path: 'signin', component: SigninComponent},
+    {path: 'add-assignment', component: AddAssignmentComponent}
 ];
     
